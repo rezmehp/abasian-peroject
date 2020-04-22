4b2f1ff2e68c5d4d4b1dcf7edd33b4a3e036ea4e
rezmeh <rezmehproject@gmail.com>
2020-04-22 19:47:11 +0430

    22/4/2020

--- tutorialvideo/views.py
+++ tutorialvideo/views.py
@@ -16,13 +16,47 @@ def tutorialvideo(request):
     reshteTahsilishows = reshteTahsili.objects.all()
     modaresinshows = modaresin.objects.all()
     coursevideoshows = coursevideo2.objects.all()
-    karbaruser1online = karbaruser1.objects.filter(username=request.user.username).values_list('reshte',flat=True)
-    reshteTahsiliid = reshteTahsili.objects.filter(reshte=karbaruser1online[0]).values_list('id',flat=True)
-    bettercoursevideoshows = coursevideo2.objects.filter(reshteTahsilifkey=reshteTahsiliid[0]).order_by('-id')[:4]
+    searchcoursevideoshows = ""
+    # سرج
+    if 'maghtan' in request.GET:
+        maghtan = request.GET['maghtan']
+        if maghtan != "مقطع تحصیلی":
+            maghtanid = maghtaTahsili.objects.filter(maghta=maghtan).values_list('id', flat=True)
+            if maghtanid:
+                searchcoursevideoshows = coursevideo2.objects.filter(maghtafkey_id=maghtanid[0])
+
+    if 'reshten' in request.GET:
+        reshten = request.GET['reshten']
+        if reshten != "رشته تحصیلی":
+            reshtenid = reshteTahsili.objects.filter(reshte=reshten).values_list('id', flat=True)
+            if reshtenid:
+                searchcoursevideoshows = coursevideo2.objects.filter(reshteTahsilifkey_id=reshtenid[0])
+
+    if 'coursen' in request.GET:
+        coursen = request.GET['coursen']
+        if coursen != "":
+            if coursen:
+                searchcoursevideoshows = coursevideo2.objects.filter(coursename__icontains=coursen)
+
+    if 'modaresn' in request.GET:
+        modaresn = request.GET['modaresn']
+        if modaresn != "":
+            modaresnid = modaresin.objects.filter(modares__icontains=modaresn).values_list('id', flat=True)
+            if modaresnid:
+                searchcoursevideoshows = coursevideo2.objects.filter(modaresinfkey__in=modaresnid)
+                
+    if request.user.username:
+        karbaruser1online = karbaruser1.objects.filter(username=request.user.username).values_list('reshte', flat=True)
+        reshteTahsiliid = reshteTahsili.objects.filter(reshte=karbaruser1online[0]).values_list('id', flat=True)
+        bettercoursevideoshows = coursevideo2.objects.filter(reshteTahsilifkey=reshteTahsiliid[0]).order_by('-id')[:4]
+    else:
+        karbaruser1online = ""
+        reshteTahsiliid = ""
+        bettercoursevideoshows = ""
     newcoursevideoshows = coursevideo2.objects.all().order_by('-id')[:8]
     context = {
-        'reshteTahsiliid':reshteTahsiliid,
-        'karbaruser1online':karbaruser1online,
+        'reshteTahsiliid': reshteTahsiliid,
+        'karbaruser1online': karbaruser1online,
         'tutorialvideoAdmins': tutorialvideoAdmins,
         'usernameshows': usernameshows,
         'karbaruser1shows': karbaruser1shows,
@@ -30,8 +64,10 @@ def tutorialvideo(request):
         'reshteTahsilishows': reshteTahsilishows,
         'modaresinshows': modaresinshows,
         'coursevideoshows': coursevideoshows,
+        'searchcoursevideoshows': searchcoursevideoshows,
         'newcoursevideoshows': newcoursevideoshows,
         'bettercoursevideoshows': bettercoursevideoshows,
+
 
     }
 
@@ -43,6 +79,6 @@ def showvideotutorial(request, coursevideo2_id):
     videoss = videos.objects.filter(coursenamefkey=coursevideo2_id)
     context = {
         'coursevideo': coursevideo,
-        'videoss':videoss
+        'videoss': videoss
     }
     return render(request, 'pages/showvideotutorial.html', context)
