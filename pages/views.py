f484f42d05e4d0c51ca22af4da72d392dac9ea8b
rezmeh <rezmehproject@gmail.com>
2020-06-01 20:52:33 +0430

    1/6/2020-2

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,46 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from tutorialapplication.models import courseapplication2,applications
+from tutorialbook.models import coursebook2,books
+from tutorialfile.models import coursefile2,files
+from tutorialvideo.models import coursevideo2,videos
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    newcourseapplications = courseapplication2.objects.all().order_by('-id')[:3]
+    newcoursebooks = coursebook2.objects.all().order_by('-id')[:3]
+    newcoursefiles = coursefile2.objects.all().order_by('-id')[:3]
+    newcoursevideos = coursevideo2.objects.all().order_by('-id')[:3]
+    countapplications = applications.objects.all().count()
+    countbooks = books.objects.all().count()
+    countfiles = files.objects.all().count()
+    countvideos = videos.objects.all().count()
+
+    context={
+
+        'newcourseapplications':newcourseapplications,
+        'newcoursebooks':newcoursebooks,
+        'newcoursefiles':newcoursefiles,
+        'newcoursevideos':newcoursevideos,
+        'countapplications':countapplications,
+        'countbooks':countbooks,
+        'countfiles':countfiles,
+        'countvideos':countvideos,
+    }    
+
+    
+    return render(request, 'pages/index.html',context)
+
+
+
+
+
+
+
+
+
+
 
 
 
